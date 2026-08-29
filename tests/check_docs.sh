ef2337edca4b5d823a0fd29b1b1535cb964c0ad6
Michael <x30827pos@gmail.com>
2026-08-29 16:20:28 +0300

    fix: use portable docs checks

--- tests/check_docs.sh
+++ tests/check_docs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 for heading in Installation Configuration Keymaps API Limitations Troubleshooting; do
-  rg -q "^## ${heading}$" README.md
+  grep -Fqx "## ${heading}" README.md
 done
-rg -q 'Xpos587/elio.nvim' README.md
-rg -q 'chooser-file' README.md
-rg -q 'cwd-file' README.md
+grep -Fq 'Xpos587/elio.nvim' README.md
+grep -Fq 'chooser-file' README.md
+grep -Fq 'cwd-file' README.md
